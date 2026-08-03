65cc25b47da734f0cda7ad90968ec72f601c30ad
John Lawson <lawsonjohnr@gmail.com>
2026-08-03 12:42:14 -0500

    Added routes to user in backend

--- backend/api/users.js
+++ backend/api/users.js
@@ -4,13 +4,30 @@ import requireBody from "../middleware/requireBody";
 
 const usersRouter = Router();
 
+
 usersRouter.post('/register', requireBody, async (req, res, next) => {
     try {
-        const user = req.body;
+        const {username, password} = req.body;
+        const user = await createUser(username, pasword);
 
-        res.send(await create_user(user));
+        const token = await createToken({id: user.id})
+       res.status(201).send(token);
     }    
     catch {
         
+    }
+});
+
+usersRouter.get('/login', requireBody, async (req, res, next) => {
+    try {
+        const {username, password} = req.body;
+        const user = await userLogin(username, password);
+
+         if (!user) return res.status(401).send("Invalid username or password.");
+    const token = await createToken({ id: user.id });
+    res.send(token);
+    }    
+    catch(error) {
+        console.log(error);
     }
 });
