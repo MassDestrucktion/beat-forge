8e70a43fba085cedc688add4d85127e86894a052
John Lawson <lawsonjohnr@gmail.com>
2026-08-05 13:04:03 -0500

    Thur work

--- frontend/src/userPage.jsx
+++ frontend/src/userPage.jsx
@@ -1,12 +1,19 @@
 
 export default function userPage() {
     return(
-        <div>
+        <div className="profileGrid">
+            <image className="pgrid1" src="" alt="MyIMG" />
+            <div className="pgrid2">
             <h1>Profile: </h1>
-            <h3>usnename</h3>
+            <h3 >usnename</h3>
             <h3>user_Id</h3>
-            <ul>
-                <li></li>
+            </div>
+            <ul className="pgrid3">
+                <li>track1</li>
+                <li>track1</li>
+                <li>track1</li>
+                <li>track1</li>
+                <li>track1</li>
             </ul>
             <button>Start New Project</button>
 
