357aaa039c36f00064ae0777031d157aca3b913c
Jacob Harris <jacobwho07@gmail.com>
2026-08-05 14:32:47 -0500

    jwt added

--- backend/api/users.js
+++ backend/api/users.js
@@ -3,6 +3,7 @@ import { Router } from "express";
 import requireBody from "../middleware/requireBody.js";
 import { createUser } from "../db/queries/users.js";
 import { userLogin } from "../db/queries/users.js";
+import { createToken } from "../jwt/jwt.js";
 
 const usersRouter = Router();
 
@@ -36,8 +37,8 @@ usersRouter.post('/login', requireBody(["username", "password"]), async (req, re
         const user = await userLogin(username, password);
 
          if (!user) return res.status(401).send("Invalid username or password.");
-    //const token = await createToken({ id: user.id });
-    res.send(user);
+    const token = await createToken({ id: user.id });
+    res.send(token);
     }    
     catch(error) {
         console.log(error);
