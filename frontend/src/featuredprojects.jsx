1768f3ce0bd8fc8c9a96036196b659cf9abb1d68
Jacob Harris <jacobwho07@gmail.com>
2026-08-24 10:50:57 -0500

    prod 2

--- frontend/src/featuredprojects.jsx
+++ frontend/src/featuredprojects.jsx
@@ -6,13 +6,13 @@ import UserSearch from "./components/userSearch";
 
 import "./styles/userPage.css";
 
-import default_Pic from "../public/media/default_Pic.jpg";
-import cool from "../public/media/cool.jpg";
-import glasses from "../public/media/glasses.jpg";
-import headphones from "../public/media/DarkHeadphones.jpg";
-import gorilla from "../public/media/Gorilla.jpg";
-import AVDreds from "../public/media/AVDreds.png";
-import BlockParty from "../public/media/BlockParty.jpg";
+import default_Pic from "/media/default_Pic.jpg";
+import cool from "/media/cool.jpg";
+import glasses from "/media/glasses.jpg";
+import headphones from "/media/DarkHeadphones.jpg";
+import gorilla from "/media/Gorilla.jpg";
+import AVDreds from "/media/AVDreds.png";
+import BlockParty from "/media/BlockParty.jpg";
 
 const profilePictures = [
   { id: "default_pic", src: default_Pic },
