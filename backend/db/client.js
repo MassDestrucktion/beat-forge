0a86fe52d6c5921cfb73d93939a3aed17bbcae08
Jacob Harris <jacobwho07@gmail.com>
2026-08-24 11:12:07 -0500

    fixed api queries

--- backend/db/client.js
+++ backend/db/client.js
@@ -4,8 +4,15 @@ const { Pool } = pg;
 
 const connectionString = process.env.DATABASE_CONNECTION;
 
+if (!connectionString) {
+  throw new Error("DATABASE_CONNECTION environment variable is missing");
+}
+
 const db = new Pool({
   connectionString,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 export default db;
