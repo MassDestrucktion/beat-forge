2196f349c22054176b328026781384860b9d06cf
Jacob Harris <jacobwho07@gmail.com>
2026-08-24 10:43:38 -0500

    further import changes

--- frontend/src/components/followingItem.jsx
+++ frontend/src/components/followingItem.jsx
@@ -1,11 +1,11 @@
 import { Link } from "react-router-dom";
-import cool from "../media/cool.jpg";
-import glasses from "../media/glasses.jpg";
-import headphones from "../media/DarkHeadphones.jpg";
-import gorilla from "../media/Gorilla.jpg";
-import AVDreds from "../media/AVDreds.png";
-import default_Pic from "../media/default_Pic.jpg";
-import BlockParty from "../media/BlockParty.jpg";
+import cool from "../public/media/cool.jpg";
+import glasses from "../public/media/glasses.jpg";
+import headphones from "../public/media/DarkHeadphones.jpg";
+import gorilla from "../public/media/Gorilla.jpg";
+import AVDreds from "../public/media/AVDreds.png";
+import default_Pic from "../public/media/default_Pic.jpg";
+import BlockParty from "../public/media/BlockParty.jpg";
 
 const profilePictures = [
   { id: "default_pic", src: default_Pic },
